e9e2138249b62ca40849f4120083892a3154e78d
Andrey <andslob@inbox.ru>
2022-12-09 12:11:22 +0500

    222

--- Seminar2/TaskDop/Program.cs
+++ Seminar2/TaskDop/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// task dop
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), max1 = n, max2 = -1;
+while (n != 0)
+{
+    n = Convert.ToInt32(Console.ReadLine());
+    if (max1 < n)
+    {
+        max2 = max1;
+        max1 = n;
+    }
+    else if (max2 < n)
+    {
+        max2 = n;
+    }
+}
+Console.WriteLine(max2);
