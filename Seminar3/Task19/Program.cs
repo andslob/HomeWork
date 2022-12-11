4350b35533c63f8a6cbd08699c569c8571a38a0b
Andrey <andslob@inbox.ru>
2022-12-11 16:59:01 +0500

    19

--- Seminar3/Task19/Program.cs
+++ Seminar3/Task19/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task 19
+
+Console.Clear();
+Console.Write("Введите пятизначное число: ");
+string? n = Console.ReadLine(); 
+ 
+if (n[0] == n[4] && n[1] == n[3])
+    Console.WriteLine("Полиндром ");
+else
+    Console.WriteLine("Не полиндром");
