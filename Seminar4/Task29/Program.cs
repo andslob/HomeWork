b1e79244082f43f58ed1c5b95f3c1f474bd9efd1
Andrey <andslob@inbox.ru>
2022-12-13 15:28:23 +0500

    29

--- Seminar4/Task29/Program.cs
+++ Seminar4/Task29/Program.cs
@@ -3,13 +3,12 @@
 Console.Clear();
 Console.Write("Введите кол-во элементов массива: ");
 int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите элементы массива: ");
-string? n = Console.ReadLine();
-double[] array = new double[n];
-for (int i = 0; i < array.Length; i++)
-{
-    //                                              * (end - begin) + begin
-    array[i] = Math.Round(new Random().NextDouble() * (50 - 10) + 10, 2); // [10, 50]
-}
+Console.WriteLine("Введите значения элементов массива: ");
 
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+{                                          
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
 Console.WriteLine($"[{string.Join(", ", array)}]");
+
