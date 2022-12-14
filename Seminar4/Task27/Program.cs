403a0288751b0585928b028978c2682b47c04579
Andrey <andslob@inbox.ru>
2022-12-14 13:50:33 +0500

    27

--- Seminar4/Task27/Program.cs
+++ Seminar4/Task27/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿// task 27
+int Sum(int n)
+{
+
+    int count = Convert.ToString(n).Length;
+    int res = 0;
+    int result = 0;
+
+    for (int i = 0; i < count; i++)
+    {
+        res = n - n % 10;
+        result = result + (n - res);
+        n = n / 10;
+    }
+    return result;
+}
+Console.Clear();
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = Sum(n);
+Console.WriteLine("Сумма: " + sum);
