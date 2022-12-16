a4763c902732b4527c7a33bbdb15e3b1d5dd3779
Andrey <andslob@inbox.ru>
2022-12-16 12:38:40 +0500

    td1

--- Seminar4/TaskDop1/Program.cs
+++ Seminar4/TaskDop1/Program.cs
@@ -1,4 +1,7 @@
 ﻿// Task Dop1
+// string[] s = Console.ReadLine().Split("а"); // 
+// for (int i = 0; i < s.Length; i++)
+//     Console.WriteLine(s[i]);
 
 
 
@@ -15,3 +18,34 @@
 // }
 // Console.WriteLine($"[{string.Join(", ", array)}]");
 
+
+// int[] s = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+// for (int i = 0; i < s.Length; i++)
+//     Console.WriteLine(s[i] * 10);
+
+
+// int[] s = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+// foreach (int element in s)
+//     Console.WriteLine(element * 2);
+int n = Convert.ToInt32(Console.ReadLine());
+int[] numbers = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int k = Convert.ToInt32(Console.ReadLine());
+int[] result = new int[numbers.Length];
+k %= n;
+if (k > 0)
+{
+    for (int i = 0; i < k; i++)
+        result[i] = numbers[numbers.Length - k + i];
+    for (int i = 0; i < numbers.Length - k; i++)
+        result[k + i] = numbers[i];
+    Console.WriteLine($"{string.Join(" ", result)}");
+}
+else
+{
+    k = k * (-1);
+    for (int i = 0; i < k; i++)
+        result[numbers.Length - k + i] = numbers[i];
+    for (int i = 0; i < numbers.Length - k; i++)
+        result[i] = numbers[k + i];
+    Console.WriteLine($"{string.Join(" ", result)}");
+}
