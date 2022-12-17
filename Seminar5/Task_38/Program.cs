73f72a26605471d7c8f0def0f124f442245c264d
Andrey <andslob@inbox.ru>
2022-12-18 01:13:25 +0500

    38

--- Seminar5/Task_38/Program.cs
+++ Seminar5/Task_38/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task 38
+
+void InputArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-100, 100);
+}
+void PrintArray(double[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();
+}
+Console.Clear();
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+InputArray(numbers);
+PrintArray(numbers);
+double min = Convert.ToDouble(Console.ReadLine());
+double max = Convert.ToDouble(Console.ReadLine());
+
+for (int n = 0; n < numbers.Length; n++)
+{
+    if (min > numbers[n]) 
+        min = numbers[n];
+        
+    if (max < numbers[n]) 
+        max = numbers[n];
+}
+Console.WriteLine($"{max - min}");
