c1cbc641bfff8bfc84405cfc1c9d093f4ba9fedb
Andrey <andslob@inbox.ru>
2022-12-19 20:45:21 +0500

    43

--- Seminar6/Task_43/Program.cs
+++ Seminar6/Task_43/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+double x = 0;
+double y = 0;
+if ((k1 == k2) && (b1 == b2))
+{
+    Console.Write("Прямые параллельны");
+}
+else if (k1 == k2)
+{
+    Console.Write("Прямые сопадают");
+}
+else x = ((b2 - b1) / (k1 - k2));
+     y = (k1 * (b2 - b1) / (k1 - k2) + b1);
+Console.WriteLine($"{x}, {y}");
