3b3846addddf151e1677f07dc0c09de763fd6045
Andrey <andslob@inbox.ru>
2022-12-19 16:46:21 +0500

    41

--- Seminar6/Task_41/Program.cs
+++ Seminar6/Task_41/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void InputArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = Convert.ToDouble(Console.ReadLine());
+}
+void PrintArray(double[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();
+}
+Console.Clear();
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+InputArray(numbers);
+PrintArray(numbers);
+int result = 0;
+for (int n = 0; n < numbers.Length; n++)
+{
+
+    if (numbers[n] > 0)
+        result = result + 1;
+}
+Console.WriteLine(result);
