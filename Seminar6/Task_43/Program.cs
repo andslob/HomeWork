724f5dbea8aa3544ed489a82e27cd3aa89485f25
Andrey <andslob@inbox.ru>
2022-12-20 10:58:58 +0500

    43

--- Seminar6/Task_43/Program.cs
+++ Seminar6/Task_43/Program.cs
@@ -18,5 +18,5 @@ else if (k1 == k2)
     Console.Write("Прямые сопадают");
 }
 else x = ((b2 - b1) / (k1 - k2));
-     y = (k1 * (b2 - b1) / (k1 - k2) + b1);
+y = (k1 * (b2 - b1) / (k1 - k2) + b1);
 Console.WriteLine($"{x}, {y}");
