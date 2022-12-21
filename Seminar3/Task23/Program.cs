b6e53ee35526a9831a0282a17677ecb5050a7dad
Andrey <andslob@inbox.ru>
2022-12-21 11:33:51 +0500

    pokm

--- Seminar3/Task23/Program.cs
+++ Seminar3/Task23/Program.cs
@@ -1,8 +1,7 @@
 ﻿// Task 23
 Console.Clear();
 Console.Write("Введите число A: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число B: ");
-int b = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
 for (int i = 1; i <= n; i++)
     Console.Write($"{i * i * i} ");
--- Seminar5/Task_38/Program.cs
+++ Seminar5/Task_38/Program.cs
@@ -32,3 +32,6 @@ for (int n = 0; n < numbers.Length; n++)
              
 }
 Console.WriteLine(max - min);
+
+       // double n = new Random().NextDouble() * (end - begin) + begin
+// Console.WriteLine(new Random().NextDouble() * (10 + 10) - 10); // [-10, 10]
