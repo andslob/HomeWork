e2fe3886d2e7c0304c38b88c8ba083c0e73351a1
Andrey <andslob@inbox.ru>
2022-12-24 09:12:21 +0500

    47

--- Seminar6/TaskDop1/Program.cs
+++ Seminar6/TaskDop1/Program.cs
@@ -32,3 +32,15 @@ double c = Dist(x1, y1, x3, y3);
 Console.Write($"Стороны равны: {a}  {b}  {c}");
 double result = Area (a, b, c);
 Console.WriteLine($"\nПлощадь треугольника равна: {result}");
+
+
+
+// Console.Clear();
+// double[] coord = Console.ReadLine().Split(" ").Select(x => Convert.ToDouble(x)).ToArray();
+// double x1 = coord[0], y1 = coord[1], x2 = coord[2], y2 = coord[3], x3 = coord[4], y3 = coord[5];
+// double AB = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+// double BC = Math.Sqrt(Math.Pow(x1 - x3, 2) + Math.Pow(y1 - y3, 2));
+// double AC = Math.Sqrt(Math.Pow(x2 - x3, 2) + Math.Pow(y2 - y3, 2));
+// double p = (AB + BC + AC) / 2;
+// double S = Math.Sqrt(p * (p - AB) * (p - BC) * (p - AC));
+// Console.WriteLine(Math.Round(S, 2));
--- Seminar7/Task_47/Program.cs
+++ Seminar7/Task_47/Program.cs
@@ -1,11 +1,11 @@
 ﻿// 47
-void InputMatrix(int[,] matrix)
+void InputMatrix(double[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i, j] = new Random().Next(-10, 11); // [-10, 10]
+            matrix[i, j] = Math.Round(new Random().NextDouble() * (10 + 10 - 10), 1); // [-10, 10]
             Console.Write($"{matrix[i, j]} \t");
         }
         Console.WriteLine();
@@ -15,5 +15,5 @@ void InputMatrix(int[,] matrix)
 Console.Clear();
 Console.Write("Введите размеры матрицы: ");
 int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
-int[,] matrix = new int[size[0], size[1]];
+double[,] matrix = new double[size[0], size[1]];
 InputMatrix(matrix);
