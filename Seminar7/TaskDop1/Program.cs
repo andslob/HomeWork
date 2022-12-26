3c6c895b31438d576596abba88a6c24ed01093b6
Andrey <andslob@inbox.ru>
2022-12-26 09:09:08 +0500

    транспонирование

--- Seminar7/TaskDop1/Program.cs
+++ Seminar7/TaskDop1/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Транспонирование
+// Транспонирование - 4
+// (Время: 1 сек. Память: 16 Мб Сложность: 15%)
+// Задана целочисленная матрица, состоящая из N строк и M столбцов. Требуется транспонировать ее относительно горизонтали.
+
+// Входные данные
+// Первая строка входного файла INPUT.TXT содержит два натуральных числа N и M – количество строк и столбцов матрицы. В каждой из последующих N строк записаны M целых чисел – элементы матрицы. Все числа во входных данных не превышают 100 по абсолютной величине.
+
+// Выходные данные
+// В выходной файл OUTPUT.TXT выведите матрицу, полученную транспонированием исходной матрицы относительно горизонтали.
+
+// Пример
+// №	INPUT.TXT	OUTPUT.TXT
+// 1	3 4
+// 5 9 2 6
+// 6 2 4 3
+// 1 2 8 7	1 2 8 7
+// 6 2 4 3
+// 5 9 2 6
+void InputMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(-10, 11); // [-10, 10]
+    }
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+void ReleaseMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0) / 2; i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int temp = matrix[i, j];
+            matrix[i, j] = matrix[matrix.GetLength(0) - 1 - i, j];
+            matrix[matrix.GetLength(0) - 1 - i, j] = temp;
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine("Начальный массив:");
+PrintMatrix(matrix);
+ReleaseMatrix(matrix);
+Console.WriteLine("Конечный массив:");
+PrintMatrix(matrix);
