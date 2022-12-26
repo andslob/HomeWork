4e46b17a6b45a5a4ddd274ecc5f46f01e760d6c2
Andrey <andslob@inbox.ru>
2022-12-26 08:41:02 +0500

    52

--- Seminar7/Task_52/Program.cs
+++ Seminar7/Task_52/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 52
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 11); // [-10, 10]
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void SumColumnMatrix(int[,] matrix)
+{
+    
+    double result = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {   
+            {
+                sum += matrix[j, i];
+                result = Math.Round(sum / matrix.GetLength(0), 1);
+                                
+            }
+            
+        }
+        Console.WriteLine($"{sum}, {result}");
+    }
+    
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine();
+SumColumnMatrix(matrix);
