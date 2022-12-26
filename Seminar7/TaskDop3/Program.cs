66cdb54499d721d40456065ec376377cad997021
Andrey <andslob@inbox.ru>
2022-12-26 09:19:06 +0500

    d3

--- Seminar7/TaskDop3/Program.cs
+++ Seminar7/TaskDop3/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №364. Заполнение диагоналями
+// Даны числа n и m. Создайте массив A[n][m] и заполните его, как показано на примере.
+// Входные данные
+// Программа получает на вход два числа n и m.
+
+// Выходные данные
+// Программа должна вывести  полученный массив.
+
+// Примеры
+// входные данные
+// 4 10
+// выходные данные
+//   0  1  3  6 10 14 18 22 26 30
+//   2  4  7 11 15 19 23 27 31 34
+//   5  8 12 16 20 24 28 32 35 37
+//   9 13 17 21 25 29 33 36 38 39
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+int number = 0;
+for (int i = 1; i < matrix.GetLength(0) + matrix.GetLength(1) + 1; i++)
+{
+    for (int j = 0; j < matrix.GetLength(0); j++)
+    {
+        for (int k = 0; k < matrix.GetLength(1); k++)
+        {
+            if (j + k + 1 == i)
+            {
+                matrix[j, k] = number;
+                number++;
+            }
+        }
+    }
+}
+PrintMatrix(matrix);
