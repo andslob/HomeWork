15353091ef20042ddf780031884d7afb974a5a68
Andrey <andslob@inbox.ru>
2022-12-29 22:50:33 +0500

    66

--- Seminar9/Task_66/Program.cs
+++ Seminar9/Task_66/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 66
+int sum(int m, int n)
+{
+    if (n == m)
+        return n;
+    return sum(m, n - 1) + n;
+}
+
+
+Console.Clear();
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(sum(m, n));
