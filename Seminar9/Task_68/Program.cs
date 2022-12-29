35625e42d6354096865258b42a38330b937e0614
Andrey <andslob@inbox.ru>
2022-12-29 23:41:22 +0500

    68

--- Seminar9/Task_68/Program.cs
+++ Seminar9/Task_68/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 68
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    
+        return n + 1; 
+    
+    else if (n == 0 && m > 0)
+     
+        return Akkerman(m - 1, 1);
+    return Akkerman(m - 1, Akkerman(m, n - 1));
+
+}
+
+
+Console.Write("Введите число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Akkerman(m, n));
